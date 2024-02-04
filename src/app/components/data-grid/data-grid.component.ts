96bde90e026aed1b10d9ab04195d9229d861b551
Mert patlar <meertpatlar@gmail.com>
2024-02-04 17:25:54 +0300

    dokumentasyon icin gereksiz kodlar temizlendi

--- src/app/components/data-grid/data-grid.component.ts
+++ src/app/components/data-grid/data-grid.component.ts
@@ -59,9 +59,7 @@ export class DataGridComponent implements OnInit {
     this.socailMedias = this.socialMediaService.getSocialMedia();
   }
 
-
   // dialog methods
-
   openFilterDialog() {
     this.filterPopupVisible = true;
   }
--- src/app/services/business/social-media.service.ts
+++ src/app/services/business/social-media.service.ts
@@ -12,7 +12,6 @@ export class SocialMediaService {
     }
   }
 
-
   getSocialMedia() {
     return JSON.parse(localStorage.getItem('socialMedia') || '{}');
   }
